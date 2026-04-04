7c80ae24c818d30044200dfdab5307c618bc0d67
Luong Son Ba <2025107014@obmps.com>
2026-04-04 21:02:16 +0900

    Create new Task1

--- src/main/java/com/example/login/batch/BatchScheduler.java
+++ src/main/java/com/example/login/batch/BatchScheduler.java
@@ -6,7 +6,7 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+//abccc
 @Component
 public class BatchScheduler {
 //add plus
